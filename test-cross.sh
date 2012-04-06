d6b6df022a3f83cbbb4fb0525723b6a528e41503
TANABE Atsushi <tanabe.atsushi@nikken.co.jp>
2012-04-06 18:39:05 +0900

    rubyでエラーになったら再実行してエラーを表示する様にした．
    どこがまずいのかわからないのでは問題のため．

--- test-cross.sh
+++ test-cross.sh
@@ -7,11 +7,10 @@ new=cross.rb
 
 for ext in tex csv
 do
+  (ruby $new $stg.rb out.$ext 2> /dev/null)|| ruby $new $stg.rb out.$ext || exit
+  mv out.$ext new.$ext
   (ruby $ori $stg.rb out.$ext 2> /dev/null)|| exit
   mv out.$ext ori.$ext
-  (ruby $new $stg.rb out.$ext 2> /dev/null)|| exit
-  mv out.$ext new.$ext
-  diff ori.$ext new.$ext  && rm ori.$ext new.$ext && \
     (if [ -d out ]; then rmdir out; fi) && \
     echo Test for $ext was passed.
 done
